608d14d42d55c8f5e2274945f5f8b2914bdc686a
admirableyetti <ech.emmanuel.ch@gmail.com>
2025-03-05 20:22:55 -0600

    migrate creation to livjs@0.5.1

--- src/sections/creation.ts
+++ src/sections/creation.ts
@@ -1,13 +1,13 @@
-import { widget } from 'livjs'
 import { _card } from '#components/card.ts'
 import { _inlineCodeCard } from '#components/inline_code.ts'
 import { _code } from '#components/code.ts'
 import { _section } from '#components/section.ts'
 import { SECTIONS } from './sections'
 import { shikiCode } from '#src/components/shiki.ts'
+import { b, div, h2, p, span } from '#src/tags.ts'
 
 const examples = {
-  basic: `import { type AnyWidgetElement, App, widget } from 'livjs'
+  basic: `import { App, widget } from 'livjs'
 
 App({
   children: [
@@ -16,25 +16,44 @@ App({
     })
   ]
 })
+`,
+  sugar_tags: `import { App, tags } from 'livjs'
+
+const { div } = tags
+
+App({
+  children: [
+    div({
+      children: 'Hello World!'
+    })
+  ]
+})
 `
 }
 
 export const _creation = _section([
-  widget('h2', {
+  div({
+    children: 'Hello World!'
+  }),
+  h2({
     children: 'Creating a Livjs Application',
     id: SECTIONS.creation.id
   }),
-  widget('p', {
+  p({
     children: [
-      widget('span', {
+      span({
         children:
           'The App function is in fact a component. It search for an html element with the'
       }),
       _inlineCodeCard('id="app"'),
-      widget('b', {
+      b({
         children: 'property.'
       })
     ]
   }),
-  shikiCode(examples.basic)
+  shikiCode(examples.basic),
+  p({
+    children: 'Also you can syntax sugar html tags!'
+  }),
+  shikiCode(examples.sugar_tags)
 ])
